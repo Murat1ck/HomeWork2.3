1a2fac0f42ccc8efd5d4ec30b908e1ccdee63744
Murat1ck <muratoff101088@gmail.com>
2022-09-19 21:20:12 +0300

    Задание 8

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        var a = 640;  //общее время сотрудников
+        var b = 8; //время одного сотрудника
+        var employees = a / b; //количество сотрудников
+
+
+        System.out.print("Всего работников в компании" + " " + "–" + " " + employees + " " + "человек.");
+    }
+}
